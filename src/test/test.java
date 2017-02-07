5e3e3ce1d356c83f060e2dcf391a57773781bcee
BaeKyoungYoung <haywe@hanmail.net>
2017-02-07 15:01:55 +0900

    v1.4 edit

--- src/test/test.java
+++ src/test/test.java
@@ -2,7 +2,7 @@ package test;
 
 public class test {
 	public void main(String[] args){
-		System.out.println("Hi. This Is Test.");		
+		System.out.println("Hi. This Is Test!!");		
 		
 		System.out.println("Version:"+ (new VersionName()).getVersionName());
 		System.out.println("end of message");
@@ -10,7 +10,7 @@ public class test {
 	}
 	
 	static class VersionName{		
-		String versionName="v1.31";		
+		String versionName="v1.4";		
 		public String getVersionName(){
 			return this.versionName;
 		}
