b94da3695fe713a11deef7333acea586793c03ab
BaeKyoungYoung <haywe@hanmail.net>
2017-02-07 13:49:10 +0900

    edit fun1v2

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,18 @@
 package test;
 
-public class test {
+public class Hello {
 	public void main(String[] args){
-		System.out.println("hi"); 
+		System.out.println("hi");		
+		
+		System.out.println("Version:"+ (new VersionName()).getVersionName());
+		System.out.println("end of message");
+				
+	}
+	
+	static class VersionName{		
+		String versionName="v1.0";		
+		public String getVersionName(){
+			return this.versionName;
+		}
 	}
 }
